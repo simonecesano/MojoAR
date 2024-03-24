eab1cd8a8b82406973aea75c1c6f832e3618bc45
Simone Cesano <simonecesano@libero.it>
2024-03-24 08:47:16 +0100

    First commit

---
+++ public/components/index.vue
@@ -0,0 +1,27 @@
+<style scoped>
+</style>
+<template>
+  <div id="main">
+    <h1>these are AR.js experiments</h1>
+    <ul>
+      <li><router-link :to="{ name: 'iframear' }">AR using iframe</router-link></li>
+      <li><router-link :to="{ name: 'vuear' }">AR using vue directly</router-link></li>
+    </ul>      
+  </div>
+</template>
+<script>
+module.exports = {
+    data: function () {
+	return {
+	};
+    },
+    async mounted(){
+    },
+    destroyed: function(){
+    },
+    methods: {
+    },
+    components: {
+    },
+}
+</script>
