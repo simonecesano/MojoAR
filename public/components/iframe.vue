de37239b7569078b73bde4aea81ec34b694ac1ef
Simone Cesano <simonecesano@libero.it>
2024-03-24 08:47:26 +0100

    First commit

---
+++ public/components/iframe.vue
@@ -0,0 +1,33 @@
+<style scoped>
+    #main { padding: 0; margin: 0 }
+    .ar-frame { width: 100vw; height: 50vh; padding: 0; margin: 0 }    
+</style>
+<template>
+  <div id="main">
+    <ariframe ref="ariframe" class="ar-frame" @iframe-ready="iframeLoad"></ariframe>
+    <location></location>
+  </div>
+</template>
+<script>
+module.exports = {
+    data: function () {
+	return {
+	};
+    },
+    async mounted(){
+	var vue = this;
+    },
+    destroyed: function(){
+    },
+    methods: {
+	iframeLoad: function(e){
+	    console.log(arguments.callee.name)
+	    console.log(e);
+	}
+    },
+    components: {
+	ariframe: httpVueLoader('components/ariframe.vue'),
+	location: httpVueLoader('components/base/location.vue'),
+    },
+}
+</script>
