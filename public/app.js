d5fe0fbafcc798c9fab1b5e4c4981d3d694db2c2
Simone Cesano <simonecesano@libero.it>
2024-03-24 08:40:33 +0100

    First commit

---
+++ public/app.js
@@ -0,0 +1,50 @@
+Vue.config.ignoredElements = [
+    'a-asset-item',
+    'a-assets',
+    'a-box',
+    'a-camera',
+    'a-cubemap',
+    'a-cursor',
+    'a-curvedimage',
+    'a-cylinder',
+    'a-entity',
+    'a-gltf-model',
+    'a-image',
+    'a-light',
+    'a-plane',
+    'a-rounded',
+    'a-scene',
+    'a-sky',
+    'a-text',
+    'a-triangle'
+];
+
+console.rlog = function(){
+    axios.post('/log', JSON.stringify(arguments))
+	.then(d => console.log(d.data))
+	.catch(e => console.log(e))
+}
+
+
+var app = new Vue({
+    el: '#app',
+    router: (new VueRouter({
+	routes: [
+	    { path: '/camera/iframe/:content?',  component: httpVueLoader('components/iframe.vue'), name: "iframear" },
+	    { path: '/camera/vue',     component: httpVueLoader('components/ar.vue'), name: "vuear" },
+	    { path: '/',     component: httpVueLoader('components/index.vue') },
+    	],
+    })),
+    data(){
+	return {
+	}
+    },
+    async mounted(){
+    },
+    methods: {
+    },
+    components: {
+    },
+    filters: {
+    }    
+});
