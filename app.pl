31063e32dfa8713d07ac4a762893fe2802909c34
Simone Cesano <simonecesano@libero.it>
2024-03-29 18:16:12 +0100

    Added points API

--- app.pl
+++ app.pl
@@ -35,11 +35,40 @@ post '/log' => sub ($c) {
     $c->render(json => { ok => time });
 };
 
-get "/points" => sub ($c) {
-    $c->render(json => $points);
+get "/api/v1/points" => sub ($c) {
+    $c->render_later;
+    $c->stash("coords" => [ split /\,/, $c->param("coords") ] || [1, 1] );
+    $c->log->info(dumper $c->stash("coords"));
+    $c->stash(radius => ($c->param("r")) || 1);
+
+    my $sparql = $c->render_to_string(template => 'around', format => 'sparql');
+    my $url = Mojo::URL->new('https://query.wikidata.org/sparql');
+    $url->query({ query => $sparql, format => "json" });
+
+    my $tx = $c->ua->build_tx(GET => $url);
+    $c->ua->start_p($tx)
+	->then(sub {
+		   my $tx = shift;
+		   $c->render(json => $tx->res->json->{results}->{bindings});
+	       })
 };
 
 app->start;
-__DATA__
 
-    
+__DATA__
+@@ around.sparql.ep
+% my $loc = $coords;    
+SELECT ?place ?placeCoords ?placeCategory ?placePicture ?placeLabel WHERE {
+    SERVICE wikibase:around {
+      # this is lng lat
+      ?place wdt:P625 ?coords .
+      bd:serviceParam wikibase:center "Point(<%= $loc->[0] %> <%= $loc->[1] %>)"^^geo:wktLiteral .
+      bd:serviceParam wikibase:radius "<%= $radius %>" .
+    }
+
+  ?place wdt:P31  ?placeCategory .
+  ?place wdt:P625 ?placeCoords   .
+
+  optional { SERVICE wikibase:label { bd:serviceParam wikibase:language "en, de, it, fr, es" } }
+  optional{ ?place wdt:P18 ?placePicture . }
+}
