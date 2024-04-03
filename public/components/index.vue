da8e27cacfa366714696cea7022a5ea11b9ceb2a
Simone Cesano <simonecesano@libero.it>
2024-04-03 02:22:29 +0200

    Reference working implementation

--- public/components/index.vue
+++ public/components/index.vue
@@ -5,9 +5,13 @@
     <h1>these are AR.js experiments</h1>
     <ul>
       <li><router-link :to="{ name: 'iframear' }">AR using iframe</router-link></li>
-      <li><router-link :to="{ name: 'vuear' }">AR using vue directly</router-link></li>
+      <li><router-link :to="{ name: 'vueardynamic' }">AR using vue directly</router-link></li>
+      <li><router-link :to="{ name: 'vuearstatic' }">AR using vue with pre-built component</router-link></li>
       <li><router-link :to="{ name: 'touch' }">Touch event experiment</router-link></li>
       <li><router-link :to="{ name: 'handdetection' }">Hand detection experiment</router-link></li>
+      <li><a href="/arjs.html">Totally static example<a></li>
+      <li><a href="/locex.html">Single shape example</a></li>
+      <li><a href="/aframe.html">Not even located example<a></li>
     </ul>      
   </div>
 </template>
