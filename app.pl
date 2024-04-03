617052a7ad681dbb02442af3e7dd4379f24f9bf3
Simone Cesano <simonecesano@libero.it>
2024-04-03 02:21:17 +0200

    Reference working implementation

--- app.pl
+++ app.pl
@@ -1,25 +1,24 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
-use Mojo::Util qw/dumper encode decode/;
+use Mojo::Util qw/dumper encode decode decamelize/;
 use Mojo::File qw/path/;
-
-my $log = path("log.txt")->open('w+');
-my $points = [
-	      map {
-		  for($_->[1]) {
-		      s/Point\(//;
-		      s/\)//;
-		      $_ = [ reverse split /\s/, $_ ]
-		  }
-		  $_
-	      }
-	      map { [ split /\t/, $_ ] } split /\n/, decode "UTF-8", path("points.txt")->slurp ];
+use Hash::Merge qw/merge/;
+use List::Util qw/uniq/;
 
 app->types->type(vue => 'text/plain');
 
-get '/iframe/:mode' => sub ($c) {
-    $c->stash("points" => $points);
-    $c->render(template => 'ariframe');
+my @point = reverse (45.43795891900473, 12.326937241282431);
+
+helper "get_points_p" => sub($c, $coords, $radius) {
+    $c->stash("coords" => $coords);
+    $c->stash(radius => $radius);
+
+    my $sparql = $c->render_to_string(template => 'around', format => 'sparql');
+    my $url = Mojo::URL->new('https://query.wikidata.org/sparql');
+    $url->query({ query => $sparql, format => "json" });
+
+    my $tx = $c->ua->build_tx(GET => $url);
+    return $c->ua->start_p($tx)
 };
 
 get '/page' => sub ($c) {
@@ -35,25 +34,77 @@ post '/log' => sub ($c) {
     $c->render(json => { ok => time });
 };
 
-get "/api/v1/points" => sub ($c) {
+get "/components/arstatic.vue" => sub ($c) {
+    # $c->render(json => { ok => time, location => [12.312911155168365, 45.972276416729926] });
     $c->render_later;
-    $c->stash("coords" => [ split /\,/, $c->param("coords") ] || [1, 1] );
-    $c->log->info(dumper $c->stash("coords"));
-    $c->stash(radius => ($c->param("r")) || 1);
-
-    my $sparql = $c->render_to_string(template => 'around', format => 'sparql');
-    my $url = Mojo::URL->new('https://query.wikidata.org/sparql');
-    $url->query({ query => $sparql, format => "json" });
-
-    my $tx = $c->ua->build_tx(GET => $url);
-    $c->ua->start_p($tx)
+    # san stin
+    $c->get_points_p(\@point, 2)
 	->then(sub {
 		   my $tx = shift;
-		   $c->render(json => $tx->res->json->{results}->{bindings});
+		   my $json = $tx->res->json->{results}->{bindings};
+		   $json = rollup([ map { munge($_) } $json->@* ]);
+		   $c->stash("points" => $json);
+		   $c->render(template => "arstatic", format => "vue")
+	       })
+};
+
+get "/api/v1/points" => sub ($c) {
+    $c->render_later;
+    $c->get_points_p(\@point, 2)
+	->then(sub {
+		   my $tx = shift;
+		   my $json = $tx->res->json->{results}->{bindings};
+		   $json = rollup([ map { munge($_) } $json->@* ]);
+		   $c->render(json => $json);
+	       })
+};
+get '/iframe/:mode' => sub ($c) {
+    $c->render_later;
+    $c->get_points_p(\@point, 2)
+	->then(sub {
+		   my $tx = shift;
+		   my $json = $tx->res->json->{results}->{bindings};
+		   $json = rollup([ map { munge($_) } $json->@* ]);
+		   $c->stash("points" => $json);
+		   $c->render(template => 'ariframe');
 	       })
 };
 
 app->start;
+
+sub munge {
+    my $r = shift();
+
+    $r->{decamelize(ucfirst $_) =~ s/.+_//r} = (delete $r->{$_})->{value} for grep { !/_/ } (keys $r->%*);
+
+    $r->{$_} =~ s/.+\/// for grep { $r->{$_} } qw/category place/;
+
+    $r->{$_} = [ split / /, $r->{$_} =~ s/^Point\(//r =~ s/\)$//r ] for grep { $r->{$_} } qw/coords/;
+    $r->{$_} = [ split /\s+/, ($r->{$_} || "") ] for qw/categories/;
+    return $r;
+}
+
+sub rollup {
+    my $arr = shift;
+    my $res = {};
+
+    for ($arr->@*) {
+	my $p = join "\t", $_->{place}, $_->{coords}->@*;
+	if ($res->{$p}) {
+	    $_->{categories} = [ delete $_->{category}];
+	    $res->{$p} = merge($res->{$p}, $_);
+	} else {
+	    $res->{$p} = $_;
+	    $res->{$p}->{categories} = [ delete $res->{$p}->{category}]
+	}
+    }
+    for (values $res->%*) {
+	$_->{categories} = [ uniq $_->{categories}->@* ];
+	$_->{coords} = [ @{$_->{coords}}[0, 1] ];
+    }
+    return [ sort { $a->{place} cmp $b->{place} } values $res->%* ]
+}
+
 
 __DATA__
 @@ around.sparql.ep
